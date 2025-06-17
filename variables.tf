e8773eadd55f698c07fd7d60fca247dcfa149680
Janardhan <janardhanj265@gmail.com>
2025-06-17 22:39:41 +0530

    ec2 test

--- variables.tf
+++ variables.tf
@@ -10,6 +10,9 @@ variable "instance_type" {
     type = string
     description = "Instance size"
 
+# enforcing standards through modules(i.e. the values should be only three that we are
+# specified only in condition, untill unless if somebody tries toii give another value
+# it doesn't take that)
     validation {
         condition     = contains(["t3.micro", "t3.small", "t3.medium"], var.instance_type)
         error_message = "Valid values for instance_type are: t3.micro, t3.small, t3.medium"
